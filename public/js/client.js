f1e0c5678b1c20f649150a163ec228412d512182
Vladimir Tkachenko <tva@lessons-tva.info>
2019-02-21 10:20:13 +0200

    Initial commit

---
+++ public/js/client.js
@@ -0,0 +1,15 @@
+﻿ var socket = io();
+      $('form').submit(function()
+      {
+        <!-- Отправка сообщения --> 
+        socket.emit('chat message', $('#m').val());
+        <!-- Очищаем текстовое окно сообщения --> 
+        $('#m').val('');
+        return false;
+      });
+ 
+      <!-- Приём сообщения с сервера --> 
+      socket.on('chat message', function(msg)
+      {
+        $('#messages').append($('<li>').text(msg));
+      });
---
+++ server.js
@@ -0,0 +1,25 @@
+﻿const express = require("express");
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + "/public"));
+ 
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+ 
+ 
+// socket.on - приём сообщения
+// io.emit - отправка сообщения
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+ 
+http.listen(app.get('port'), function() {
+    console.log('Server listens on port', app.get('port'));
+});
